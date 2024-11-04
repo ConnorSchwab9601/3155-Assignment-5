a62f6cb26beacc16bbb427a27b396333fda5165d
Connor Schwab <Connor.l.schwab@gmail.com>
2024-11-03 21:44:30 -0500

    feat added controllers for sandwiches and order_details, and added FastAPI routes for both

---
+++ api/controllers/order_details.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
+
+
+def create(db: Session, order_id: int, sandwich_id: int, amount: int):
+    order_detail = models.OrderDetail(order_id=order_id, sandwich_id=sandwich_id, amount=amount)
+    db.add(order_detail)
+    db.commit()
+    db.refresh(order_detail)
+    return order_detail
+
+
+def read_all(db: Session):
+    return db.query(models.OrderDetail).all()
+
+
+def read_one(db: Session, order_detail_id):
+    return db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id).first()
+
+def update(db: Session, order_detail_id: int, amount: int):
+    order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id).first()
+    if order_detail:
+        order_detail.amount = amount
+        db.commit()
+        db.refresh(order_detail)
+    return order_detail
+
+
+def delete(db: Session, order_detail_id):
+    db_order_detail = db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id)
+    db_order_detail.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
